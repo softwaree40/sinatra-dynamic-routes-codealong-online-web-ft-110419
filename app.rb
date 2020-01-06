de75a74c6bd0f2888398ea5018b0a89d46c37c7d
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-06 21:04:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,12 +18,5 @@ class App < Sinatra::Base
         @name = params[:name]
         "Goodbye, #{@name}."
     end
-     get "/multiply/:num1/:num2" do
-       
-        @num1 = params[:num1] 
-        @num2 = params[:num2]
-        @num1 * @num2
-        
-     end
-
+    
 end
