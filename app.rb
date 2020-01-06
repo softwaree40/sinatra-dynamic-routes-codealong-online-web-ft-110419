d01c984bc22927ef90e6b9b4d11d7decc09f02f4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-06 21:01:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,8 +22,8 @@ class App < Sinatra::Base
        
         @num1 = params[:num1] 
         @num2 = params[:num2]
-        product = @num1 * @num2
-        product
+        @num1 * @num2
+        
      end
 
 end
